ce72d26c0a2ad4ae2e8e1654cd92185aa2b3aa77
fscarlier <fscarlier@gmail.com>
2021-09-14 10:52:49 +0200

    Changed dir and file naming, fixed imports

---
+++ fsf/elements.py
@@ -0,0 +1,424 @@
+import copy
+import at
+import numpy as np
+import lattice.lattice_conversion_functions as lcf
+import lattice.element_conversion_functions as ecf
+
+
+class Element:
+    """
+    Base element class  
+    """
+
+    def __init__(self, name, **kwargs):
+        """
+        Args:
+            name : string
+                name of element
+        
+        Key Args:
+            length: float
+                length of element [m]
+        """
+        self.name = name
+        self.length = kwargs.pop('length', 0.0)
+        self.parent = kwargs.pop('parent', self.__class__.__name__)
+        self.update(**kwargs)
+
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+
+    def __str__(self):
+        args_dict = vars(self).items()
+        args_str = [f'{k}={v}' for k,v in args_dict if k!= 'name']
+        return f"{self.__class__.__name__}('{self.name}', {', '.join(args_str)})"
+
+
+    def __repr__(self):
+        args_dict = vars(self).items()
+        args_str = [f'{k}={v}' for k,v in args_dict if k!= 'name']
+        return f"{self.__class__.__name__}('{self.name}', {', '.join(args_str)})"
+
+
+    def update(self, **kwargs):
+        for (key, value) in kwargs.items():
+            setattr(self, key, value)
+
+
+    def items(self):
+        """Iterates through the data members including slots and properties"""
+        # Get attributes
+        for k, v in vars(self).items():
+            yield k, v
+        # Get slots and properties
+        for k, v in getmembers(self.__class__, isdatadescriptor):
+            if not k.startswith('_'):
+                yield k, getattr(self, k)
+
+
+    @property
+    def position(self):
+        return self.pos 
+
+
+    @position.setter
+    def position(self, position):
+        self.pos = position
+
+
+    @classmethod
+    def from_cpymad(cls, element):
+        """ 
+        Create specific Element instance from cpymad element
+        """
+        return ecf.from_cpymad(cls, element)
+
+
+    def to_cpymad(self, madx):
+        """ 
+        Create cpymad element in madx instance from Element
+        """
+        return ecf.to_cpymad(self, madx)
+
+
+    @classmethod
+    def from_pyat(cls, element):
+        """ 
+        Create specific Element instance from pyAT element
+        """
+        return ecf.from_pyat(cls, element)
+
+
+    def to_pyat(self):
+        """ 
+        Create pyAT Element instance from element
+        """
+        if self.__class__.__name__ == 'Rbend':
+            sbend = self.convert_to_sbend()
+            return ecf.to_pyat(sbend)
+        else:
+            return ecf.to_pyat(self)
+
+
+class Drift(Element):
+    """
+    Drift element class
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+        assert self.length >= 0.0, f"Drift {name} has negative length"
+
+
+class Marker(Element):
+    """
+    Marker element class
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+        assert self.length == 0.0, f"Marker {name} has non-zero length"
+
+
+class Sbend(Element):   
+    """
+    Sbend element class
+    """
+    def __init__(self, name, **kwargs):
+        """
+        Args:
+            name : string
+                name of element
+        
+        Key Args:
+            length: float
+                arclength of element [m]
+        """
+        super().__init__(name, **kwargs)
+        self.chord_length = kwargs.pop('chord_length', self._calc_chordlength())
+        assert np.isclose(self.length, self._calc_arclength(), rtol=1e-8), f"{self.length},  {self._calc_arclength()}"
+
+    
+    def _calc_arclength(self) :
+        """
+        Calculate arclength from angle and chordlength
+        
+        Returns:
+            Float with arclength
+        """
+        return (self.angle*self.chord_length)/(2*np.sin(self.angle/2.))
+
+
+    def _calc_chordlength(self) :
+        """
+        Calculate chordlength from angle and arclength
+        
+        Returns:
+            Float with chordlength 
+        """
+        return self.length*(2*np.sin(self.angle/2.))/self.angle
+
+
+    def convert_to_rbend(self):
+        """
+        Convert Sbend element to Rbend element
+        
+        Returns:
+            Rbend instance of element 
+        """
+        kwargs = copy.copy(vars(self))
+        kwargs['arc_length'] = kwargs.pop('length')
+        kwargs['length'] = kwargs.pop('chord_length')
+        if 'e1' in kwargs:
+            kwargs['e1'] = kwargs.pop('e1')-self.angle/2.
+        if 'e2' in kwargs:
+            kwargs['e2'] = kwargs.pop('e2')-self.angle/2.
+        kwargs.pop('name')
+        return Rbend(self.name, **kwargs) 
+
+
+class Rbend(Element):   
+    """
+    Rbend element class
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+        self.arc_length = kwargs.pop('arc_length', self._calc_arclength())
+        assert np.isclose(self.length, self._calc_chordlength(), rtol=1e-8)
+
+     
+    def _calc_arclength(self) :
+        """
+        Calculate arclength from angle and chordlength
+        
+        Returns:
+            Float with arclength
+        """
+        return (self.angle*self.length)/(2*np.sin(self.angle/2.))
+
+
+    def _calc_chordlength(self) :
+        """
+        Calculate chordlength from angle and arclength
+        
+        Returns:
+            Float with chordlength 
+        """
+        return self.arc_length*(2*np.sin(self.angle/2.))/self.angle
+
+
+    def convert_to_sbend(self):
+        """
+        Convert Rbend element to Sbend element
+        
+        Returns:
+            Sbend instance of element 
+        """
+        kwargs = copy.copy(vars(self))
+        kwargs['chord_length'] = kwargs.pop('length')
+        kwargs['length'] = kwargs.pop('arc_length')
+        if 'e1' in kwargs:
+            kwargs['e1'] = kwargs.pop('e1')+self.angle/2.
+        if 'e2' in kwargs:
+            kwargs['e2'] = kwargs.pop('e2')+self.angle/2.
+        kwargs.pop('name')
+        return Sbend(self.name, **kwargs) 
+
+
+class Multipole(Element):
+    """
+    Multipole element class
+    """
+    def __init__(self, name, **kwargs):
+        """
+        Setting multipole strengths. knl and ksl have precedence over k1, k2, k3
+        """
+        self.knl = np.array(kwargs.pop('knl', [kwargs.pop('k0', 0.0), 
+                                               kwargs.pop('k1', 0.0), 
+                                               kwargs.pop('k2', 0.0), 
+                                               kwargs.pop('k3', 0.0)]))
+        self.ksl = np.array(kwargs.pop('ksl', [kwargs.pop('k0s', 0.0), 
+                                               kwargs.pop('k1s', 0.0), 
+                                               kwargs.pop('k2s', 0.0), 
+                                               kwargs.pop('k3s', 0.0)]))
+        super().__init__(name, **kwargs)
+
+
+class Quadrupole(Multipole):
+    """
+    Quadrupole element class
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class Sextupole(Multipole):
+    """
+    Sextupole element class
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class Octupole(Multipole):
+    """
+    Octupole element class
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class RFCavity(Element):
+    """
+    RFCavity element class
+    """
+    def __init__(self, name, **kwargs):
+        self.volt = kwargs.pop('volt', 0.0)
+        self.freq = kwargs.pop('freq', 0.0)
+        super().__init__(name, **kwargs)
+
+
+class Collimator(Drift):
+    """
+    Collimator element class
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class Solenoid(Element):
+    """
+    Solenoid element class
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+    
+class BeamBeam(Element):
+    """
+    Beam-beam element class
+    
+    charge : int
+        charge of particles in the opposite beam
+    sigx : float
+        horizontal beam size of opposite beam
+    sigy : float
+        vertical beam size of opposite beam
+    dx : float
+        horizontal orbit offset of opposite beam
+    dy : float
+        vertical orbit offset of opposite beam
+    shape : str
+        radial density shape of the opposite beam, default='gaussian'
+    """
+    def __init__(self, name, **kwargs):
+        self.charge = kwargs.pop('charge', 1)
+        self.sigx = kwargs.pop('sigx', 1)
+        self.sigy = kwargs.pop('sigy', 1)
+        self.dx = kwargs.pop('dx', 0)
+        self.dy = kwargs.pop('dy', 0)
+        self.shape = kwargs.pop('shape', 'gaussian')
+        super().__init__(name, **kwargs)
+
+
+class DipoleEdge(Element):
+    """
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class ClosedOrbitCorrector(Element):
+    """
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class TransverseKicker(Element):
+    """
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class TravellingWaveCavity(Element):
+    """
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class ThinRFMultipole(Element):
+    """
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class CrabCavity(Element):
+    """
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class ACDipole(Element):
+    """
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class ElectrostaticSeparator(Element):
+    """
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class BeamPositionMonitor(Element):
+    """
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class Instrument(Element):
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class Placeholder(Element):
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+class ArbitraryMatrixElement(Element):
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+
+CPYMAD_TO_FFF_MAP = {'marker': Marker, 'drift':  Drift, 'rbend':  Rbend, 
+                     'sbend':  Sbend, 'quadrupole': Quadrupole, 'sextupole': Sextupole, 
+                     'collimator': Collimator, 'rfcavity': RFCavity}
+
+
+FFF_TO_CPYMAD_MAP = {'Marker': 'marker', 'Drift': 'drift',  'Rbend': 'rbend',  
+                     'Sbend': 'sbend',  'Quadrupole': 'quadrupole', 'Sextupole': 'sextupole',  
+                     'Collimator': 'collimator', 'RFCavity': 'rfcavity'} 
+
+
+PYAT_TO_FFF_MAP = {'Marker': Marker, 'Drift':  Drift, 'Dipole':  Sbend, 
+                   'Quadrupole': Quadrupole, 'Sextupole': Sextupole, 'Collimator': Collimator, 
+                   'RFCavity': RFCavity}
+
+
+FFF_TO_PYAT_MAP = {'Marker': at.lattice.elements.Marker, 'Drift': at.lattice.elements.Drift, 'Sbend': at.lattice.elements.Dipole,     
+                   'Quadrupole': at.lattice.elements.Quadrupole, 'Sextupole': at.lattice.elements.Sextupole, 
+                   'Collimator': at.lattice.elements.Collimator, 'RFCavity': at.lattice.elements.RFCavity}
+
+
+# FFF_TO_XSUITE_MAP = {'Marker': '??BeamMonitor??', 'Drift': 'Drift',  'Rbend': '',  
+#                      'Sbend': '',  'Quadrupole': 'Multipole', 'Sextupole': 'Multipole',  
+#                      'Octupole': 'Multipole',  'Collimator': 'Drift', 'RFCavity': 'Cavity'} 
---
+++ fsf/lattice_conversion_functions.py
@@ -0,0 +1,75 @@
+from cpymad.madx import Madx
+import at
+import fsf.element_conversion_functions as ecf
+
+
+def create_cpymad_from_file(sequence_path, energy, particle_type='electron', logging=True):
+    if logging:
+        madx = Madx(command_log='log.madx')
+    else:
+        madx = Madx()
+
+    madx.option(echo=False, info=False, debug=False)
+    madx.call(file=sequence_path)
+    madx.input('SET, FORMAT="25.20e";')
+    madx.command.beam(particle=particle_type, energy=energy)
+    return madx
+
+
+def create_pyat_from_file(file_path):
+    ring = at.load.matfile.load_mat(file_path, key='ring')
+    ring = at.Lattice(ring)
+    return ring
+
+
+def create_global_elements_from_cpymad(madx):
+    idx = 0
+    global_elements = []
+    while '$start' not in madx.elements[idx].name:
+        if madx.elements[idx].name not in madx.base_types:
+            gl_ele = ecf.convert_cpymad_element_to_fsf(madx.elements[idx])
+            global_elements.append(gl_ele)
+        idx += 1
+    return global_elements
+
+
+def export_to_cpymad(fsf_lattice):
+    # TODO: - Should develop complete constraint on allowed keyword arguments for cpymad
+    #       - Reproduce inheritance from global elements
+    madx = Madx(command_log='log.madx')
+    madx.option(echo=False, info=False, debug=False)
+    seq_command = ''
+    global_elements_defined = False
+    
+    try: 
+        global_elements = fsf_lattice.global_elements
+        for element in global_elements:
+            element.to_cpymad(madx)
+        global_elements_defined = True
+    except AttributeError:
+        print("No global elements in cpymad instance")
+
+    elements = fsf_lattice.sequence
+    for element in elements[1:-1]:
+        if not global_elements_defined:
+            element.to_cpymad(madx)
+            seq_command += f'{element.name}, at={element.pos}  ;\n'
+        else:
+            seq_command += f'{element.name}: {element.parent}, at={element.pos}  ;\n'
+    
+    madx.input(f'{fsf_lattice.name}: sequence, refer=centre, l={fsf_lattice.sequence[-1].pos};')
+    madx.input(seq_command)
+    madx.input('endsequence;')
+    madx.command.beam(particle='electron', energy=fsf_lattice.energy)
+    return madx
+
+
+def export_to_pyat(fsf_lattice):
+    fsf_lattice.update_harmonic_number()
+    elements = fsf_lattice.line
+    seq = []
+    for element in elements:
+        pyat_el = element.to_pyat() 
+        seq.append(pyat_el)
+    pyat_lattice = at.Lattice(seq, name=fsf_lattice.name, key='line', energy=fsf_lattice.energy*1e9)
+    return pyat_lattice
