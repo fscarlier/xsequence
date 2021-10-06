7bff0493f454a93d6ac7c780d2846bc4035f3336
Felix Simon Carlier <felix.simon.carlier@cern.ch>
2021-10-06 10:16:39 +0200

    New horizontal labels dict

--- toolkit/fcc_plots.py
+++ toolkit/fcc_plots.py
@@ -5,7 +5,29 @@ matplotlib.use('qt5agg')
 import matplotlib.pyplot as plt
 
 
-def fcc_axes():
+LABELS = {
+          'dbet'  :r'$\Delta\beta/\beta$', 
+          'dbet_p':r'$\Delta\beta/\beta$ [%]', 
+          'dbetx'  :r'$\Delta\beta_x/\beta_x$', 
+          'dbetx_p':r'$\Delta\beta_x/\beta_x$ [%]', 
+          'dbety'  :r'$\Delta\beta_y/\beta_y$', 
+          'dbety_p':r'$\Delta\beta_y/\beta_y$ [%]', 
+          'betx' :r'$\beta_x$ [m]', 
+          'bety' :r'$\beta_y$ [m]', 
+          'dispx' :r'$\eta_x$ [m]', 
+          'dispy' :r'$\eta_y$ [m]', 
+          'ddispx' :r'$\Delta\eta_x$ [m]', 
+          'ddispy' :r'$\Delta\eta_y$ [m]', 
+          'alfx' :r'$\alpha_x$ ', 
+          'alfy' :r'$\alpha_y$ ', 
+          'x' :r'$x$ [m]', 
+          'y' :r'$y$ [m]', 
+          'dx' :r'$\Delta x$ [m]', 
+          'dy' :r'$\Delta y$ [m]', 
+         }
+
+
+def fcc_axes(y_label=None):
     matplotlib.style.use('ggplot')
     matplotlib.rc('axes',edgecolor='black')
     matplotlib.rc('axes',facecolor='white')
@@ -14,6 +36,8 @@ def fcc_axes():
     fig.patch.set_facecolor('white')
     ax = plt.axes([0.17, 0.15, 0.8, 0.75])
     ax.set_xlabel('s [m]', fontsize=14)
+    if y_label:
+        ax.set_ylabel(LABELS[y_label], fontsize=14)
     lhc_length = 97756.02181750233 
     ax.set_xlim([0, lhc_length])
     ips_pos = [0, 48878.01090875525]
