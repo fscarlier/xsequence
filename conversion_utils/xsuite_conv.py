09d3d810617520528e9e078b83361b61dcc5733e
Felix Simon Carlier <felix.simon.carlier@cern.ch>
2021-10-11 14:44:27 +0200

    Commit for cleaning __pycache__

--- conversion_utils/xsuite_conv.py
+++
@@ -1,7 +0,0 @@
-"""
-Module conversion_utils.xline_conv
-------------------
-:author: Felix Carlier (fcarlier@cern.ch)
-This is a Python3 module containing functions to convert elements from and to xline.
-"""
-
