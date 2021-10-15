fae12fab99ba7dee24bff25268775be6e5a157e3
Felix Simon Carlier <felix.simon.carlier@cern.ch>
2021-10-15 16:53:38 +0200

    Global parameters now in .params dict

--- fsf/lattice.py
+++ fsf/lattice.py
@@ -18,28 +18,30 @@ class Lattice:
     """
     A class used to represent an accelerator lattice 
     """
-
     def __init__(self, name, element_list, key='line', **kwargs):
         """
         Args:
-            name : string
-                name of lattice
-            element_list: iterable
-                list of accelerator elements
-        
+            name : string, name of lattice
+            element_list: iterable, list of accelerator elements
         Key Args:
             key: string
                 'line' for element_list including explicit drifts
                 'sequence' for element_list without explicit drifts
         """
         self.name = name
-        self.key = key
-        self.energy = kwargs.pop('energy', 0.0)
-        if key == 'line':
-            self.line = element_list
-        elif key == 'sequence':
-            self.sequence = element_list
+        self.params = {'key':key, 'energy':kwargs.pop('energy', 0.0)}
+        self._set_lattice(element_list)
+        self._update(**kwargs)
 
+    def _set_lattice(self, element_list):
+        if self.params['key'] == 'line':
+            self.line = element_list
+        elif self.params['key'] == 'sequence':
+            self.sequence = element_list
+    
+    def _update(self, **kwargs):
+        for (key, value) in kwargs.items():
+            setattr(self, key, value)
 
     def _convert_line_to_sequence(self):
         """
@@ -48,7 +50,6 @@ class Lattice:
 
         elements_only = [ele for ele in self._line if ele.__class__.__name__ != 'Drift' ]
         self._sequence = elements_only
-
 
     def _convert_sequence_to_line(self):
         """
@@ -84,13 +85,10 @@ class Lattice:
             element.position = previous_end + element.length/2.
             previous_end += element.length
 
-
     def get_s_positions(self, reference='center'):
         """
         Key args:
-            reference : string
-                reference location of s position at element
-        
+            reference : string, reference location of s position at element
         Returns:
             List of longitudinal positions of each element excluding drifts
         """
@@ -101,7 +99,6 @@ class Lattice:
         elif reference == 'end': 
             return [element.end for element in self._sequence]
 
-    
     @property
     def sequence(self):
         """
@@ -109,20 +106,17 @@ class Lattice:
         """
         return self._sequence
 
-
     @sequence.setter
     def sequence(self, element_list):
         """
         Set sequence and update instance
 
         Args:
-            element_list : iterable
-                list of accelerator elements
+            element_list : iterable, list of accelerator elements
         """
         self._sequence = element_list
         self._convert_sequence_to_line()
 
-    
     @property
     def line(self):
         """
@@ -130,37 +124,34 @@ class Lattice:
         """
         return self._line
 
-
     @line.setter
     def line(self, element_list):
         """
-        Set line and update instance
+        Set line and update sequence
 
         Args:
-            element_list : iterable
-                list of accelerator elements including drifts
+            element_list : list, list of accelerator elements including drifts
         """
         self._line = element_list
         self._calc_s_positions()
         self._convert_line_to_sequence()
 
-
     @property
     def total_length(self):
         return self.line[-1].end
 
-
-    def get_element_names(self):
-        self.names = [el.name for el in self.line]
+    def get_element_names(self, key='sequence'):
+        if key == 'sequence':
+            self.names = [el.name for el in self.sequence]
+        if key == 'line':
+            self.names = [el.name for el in self.line]
         return self.names 
-
-
+        
     def get_range_s(self, start_pos, end_pos):
         seq_start = np.array(self.get_s_positions(reference='start'))
         seq_end = np.array(self.get_s_positions(reference='end'))
         idx = np.array( np.where((seq_end >= start_pos) & (seq_start <= end_pos))[0] )
         return idx, self._sequence[idx[0]:idx[-1]+1]
-
 
     def get_range_elements(self, start_element, end_element):
         start_element = self.get_element(start_element)
@@ -169,19 +160,16 @@ class Lattice:
         assert len(end_element) == 1, "Cannot find range: Multiple elements with same end name"
         return self.get_range_s(start_element[0].position, end_element[0].position)
 
-
     def update_cavity_energy(self):
         cavities = self.get_class('RFCavity')
         for cav in cavities:
-            cav.energy = self.energy
-
+            cav.energy = self.params['energy']
 
     def update_harmonic_number(self):
         cavities = self.get_class('RFCavity')
         for cav in cavities:
             # Approximation for ultr-relativistic electrons
             cav.harmonic_number = int(cav.freq*1e6/(scipy.constants.c/self.total_length))
-
 
     @classmethod
     def from_madx_seqfile(cls, seq_file, seq_name, energy, particle_type='electron'):
@@ -202,7 +190,6 @@ class Lattice:
         madx.command.beam(particle=particle_type, energy=energy)
         return cls.from_cpymad(madx, seq_name)
 
-
     @classmethod
     def from_cpymad(cls, madx, seq_name):
         """
@@ -217,7 +204,6 @@ class Lattice:
             return fsf.elements.CPYMAD_TO_FSF_MAP[base_type].from_cpymad(element)
         
         madx.use(seq_name)
-        total_length = madx.sequence[seq_name].elements[-1].at
         element_seq = list(map(convert_cpymad_element_to_fsf, 
                                madx.sequence[seq_name].elements))
         
@@ -228,28 +214,19 @@ class Lattice:
         return cls(seq_name, element_seq, key='sequence', 
                    energy=madx.sequence[seq_name].beam.energy, global_variables=variables) 
 
-
     @classmethod
     def from_pyat(cls, pyat_lattice):
-        """
-        Import lattice from pyat
-        """
         def convert_pyat_element_to_fsf(element):
             base_type = element.__class__.__name__
             return fsf.elements.PYAT_TO_FSF_MAP[base_type].from_pyat(element)
 
-        total_length = pyat_lattice.get_s_pos([-1])
         seq = []
         for el in pyat_lattice:
             new_element = convert_pyat_element_to_fsf(el)
             seq.append(new_element)
         return cls(pyat_lattice.name, seq, energy=pyat_lattice.energy*1e-9) 
 
-
     def to_cpymad(self):
-        """
-        Export lattice to cpymad
-        """
         madx = Madx()
         madx.option(echo=False, info=False, debug=False)
         seq_command = ''
@@ -262,36 +239,23 @@ class Lattice:
         madx.input(f'{self.name}: sequence, refer=centre, l={self.sequence[-1].end};')
         madx.input(seq_command)
         madx.input('endsequence;')
-        madx.command.beam(particle='electron', energy=self.energy)
+        madx.command.beam(particle='electron', energy=self.params['energy'])
         return madx
 
-
     def to_pyat(self):
-        """
-        Export lattice to pyat
-        """
         self.update_cavity_energy()
         self.update_harmonic_number()
         seq = [element.to_pyat() for element in self.line]
-        pyat_lattice = at.Lattice(seq, name=self.name, key='ring', energy=self.energy*1e9)
+        pyat_lattice = at.Lattice(seq, name=self.name, key='ring', energy=self.params['energy']*1e9)
         for cav in at.get_elements(pyat_lattice, at.RFCavity):
             cav.Frequency = cav.HarmNumber*scipy.constants.c/pyat_lattice.circumference 
         return pyat_lattice
 
-
     def to_xline(self):
-        """
-        Export lattice to pyat
-        """
-        seq = []
-        names = []
-        for element in self.sliced.line:
-            seq.append(element.to_xline())
-            names.append(element.name)
-
-        xline_lattice = xl.Line(elements=seq, element_names=names)
+        names =  self.sliced.get_element_names(key='line')
+        line = [el.to_xline() for el in self.sliced.line]
+        xline_lattice = xl.Line(elements=line, element_names=names)
         return xline_lattice
-
 
     def optics(self, engine='madx', drop_drifts=False):
         """
@@ -322,20 +286,19 @@ class Lattice:
             tw = tw.drop(tw[tw['keyword']=='drift'].index)
         return tw 
 
-    
-    def get_element(self, element_name):
+    def get_element(self, element_names):
         """
         Get list of elements matching given name 
 
         Args:
-            element_name : string
-                Name of desired element
-        
-        Returns:
+            element_name : string or list of strings, Name of desired element
+        Returns: 
             List of elements matching given name
         """
-        return [element for element in self._line if element.name == element_name]
-
+        if isinstance(element_names, str):
+            return [element for element in self._line if element.name == element_names]
+        if isinstance(element_names, list):
+            return [element for element in self._line if element.name in element_names]
 
     def remove_elements(self, index=None, names=None):
         if index:
@@ -350,7 +313,6 @@ class Lattice:
                     mask[el_idx] = False
             self.sequence = np.array(self.sequence)[mask]
 
-
     def get_class(self, class_names):
         """
         Get list of elements matching given class 
@@ -364,13 +326,11 @@ class Lattice:
         """
         return [element for element in self._line if element.__class__.__name__ in class_names]
 
-
     def convert_sbend_to_rbend(self):
         """
         Convert all rbends to sbends in sequence 
         """
         self.sequence = [element.convert_to_rbend() if element.__class__.__name__ == 'Sbend' else element for element in self.sequence]
-
 
     def convert_rbend_to_sbend(self):
         """
@@ -378,12 +338,10 @@ class Lattice:
         """
         self.sequence = [element.convert_to_sbend() if element.__class__.__name__ == 'Rbend' else element for element in self.sequence]
 
-
     def slice_lattice(self):
         thin_list = [el.slice_element() for el in self.sequence]
         self._thin_sequence = [item for sublist in thin_list for item in sublist]
         return self._thin_sequence
-
 
     @property
     def sliced(self):
@@ -393,12 +351,10 @@ class Lattice:
             self.slice_lattice()
             return Lattice(self.name, self._thin_sequence, key='sequence')
 
-
     def __str__(self):
-        args_str = [f"'{self.name}'", f"{self._sequence}", f"key='{self.key}'"] 
+        args_str = [f"'{self.name}'", f"{self._sequence}", f"key='{self.params['key']}'"] 
         return f"{self.__class__.__name__}({', '.join(args_str)})"
 
-
     def __repr__(self):
-        args_str = [f"'{self.name}'", f"{self._sequence}", f"key='{self.key}'"] 
+        args_str = [f"'{self.name}'", f"{self._sequence}", f"key='{self.params['key']}'"] 
         return f"{self.__class__.__name__}({', '.join(args_str)})"
