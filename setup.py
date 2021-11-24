5fcf092e5a60c6bed7840f2423bc95c87c9c3426
mihofer <michael.hofer@cern.ch>
2021-11-24 16:44:26 +0100

    change from xtrack to xline

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ DEPENDENCIES = [
     "pandas>=1.0",
     "matplotlib>=3.3.2",
     "cpymad>=1.8.1", 
-    "xline", 
+    "xtrack", 
     "accelerator-toolbox",
     "xdeps@https://github.com/xsuite/xdeps/tarball/main",
     "lark",
