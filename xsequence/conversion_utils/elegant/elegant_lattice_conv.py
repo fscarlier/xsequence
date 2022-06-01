f76583ea1a052bc2168febe12212757d6b41e923
mihofer <michael.hofer@cern.ch>
2022-06-01 15:02:28 +0200

    fix commas and linebreaks

--- xsequence/conversion_utils/elegant/elegant_lattice_conv.py
+++ xsequence/conversion_utils/elegant/elegant_lattice_conv.py
@@ -17,10 +17,12 @@ def to_elegant(seq_name: str, energy: float, line: Line, lte_file: Union[str, Pa
     # the thinmultipole is split into its different contribution and for each, a MULT element will be added
     # as such, the conversion from the original line would break and line has to be built anew
     element_order=f"{seq_name}: LINE=("
+    i=0
     for name, element in line.items():
         name, _, elegant_definition = element.to_elegant()
         elements_definition=f"{elements_definition}{elegant_definition}\n"
-        element_order=f"{element_order}\n{name}"
+        element_order=f"{element_order}{',' if i > 0 else ''}{name}"
+        i=i+1
     element_order=f"{element_order})"
     
     if lte_file is not None:
