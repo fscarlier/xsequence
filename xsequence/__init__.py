633ba62fb7315fe08fb9394d0df31b6129a69757
fscarlier <fscarlier@gmail.com>
2022-06-28 14:24:05 +0200

    changed __init__.py for install fix

--- xsequence/__init__.py
+++ xsequence/__init__.py
@@ -5,3 +5,10 @@ __version__ = "0.0.0"
 __author__ = "fcarlier"
 __author_email__ = "fcarlier@cern.ch"
 __license__ = "MIT"
+
+from . import helpers
+from . import elements
+from . import elements_dataclasses
+from . import lattice
+from . import lattice_baseclasses
+from . import slicing
